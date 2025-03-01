96425c8ba41a0103e0148d1bcbbef51beb3bc272
Nathaniel Kwok <natkwok902@gmail.com>
2025-03-01 12:58:26 -0500

    geojson for hazard

--- frontend/safepaws_fe/src/components/HazardList.vue
+++ frontend/safepaws_fe/src/components/HazardList.vue
@@ -1,22 +1,43 @@
+<!-- <div>
+    <h1>Hazard Test</h1>
+    <p>Click the button to fetch data with an HTTP request.</p>
+    <button @click="fetchData">Fetch data</button>
+    <pre v-if="data">{{ data }}</pre>
+  </div> -->
 <template>
-  <h1>Hazard Test</h1>
-  <p>Click the button to fetch data with an HTTP request.</p>
-  <button @click="fetchData">Fetch data</button>
-  <pre v-if="data">{{ data }}</pre>
+  <l-map style="height: 350px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-geo-json :geojson="geojson"></l-geo-json>
+  </l-map>
 </template>
 
-<script lang="ts">
+<script>
 import { fetchHazards } from '@/api/hazardApi'
+import { geoJSON, latLng } from 'leaflet'
+import { LMap, LTileLayer, LGeoJson } from '@vue-leaflet/vue-leaflet'
+
 export default {
+  name: 'hazard',
+  components: {
+    LGeoJson,
+    LTileLayer,
+  },
   data() {
     return {
-      data: null,
+      // data: null,
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 8,
+      center: [47.31322, -1.319482],
+      geojson: null,
     }
   },
   methods: {
     async fetchData() {
       try {
-        this.data = await fetchHazards()
+        // this.data = await fetchHazards()
+        this.geojson = await fetchHazards()
       } catch (error) {
         console.error('Error fetching data:', error)
       }
